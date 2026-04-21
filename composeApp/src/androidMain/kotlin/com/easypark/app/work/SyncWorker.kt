e740198b05cb99447b0017d31338d51749785859
Ruben Cordero <garu1812@gmail.com>
2026-04-21 14:59:48 -0400

    FireBase

---
+++ composeApp/src/androidMain/kotlin/com/easypark/app/work/SyncWorker.kt
@@ -0,0 +1,32 @@
+package com.easypark.app.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.easypark.app.findparking.domain.usecase.GetParkingsUseCase
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class SyncWorker(
+    appContext: Context,
+    workerParameters: WorkerParameters
+) : CoroutineWorker(appContext, workerParameters), KoinComponent {
+
+    // Inyectando el caso de uso directamente desde commonMain usando Koin
+    private val getParkingsUseCase: GetParkingsUseCase by inject()
+
+    override suspend fun doWork(): Result {
+        println("Ejecutando instrucción para sincronizar parqueos...")
+        
+        return try {
+            // Ejecutamos el caso de uso
+            val parkings = getParkingsUseCase.invoke()
+            println("Datos sincronizados: ${parkings.size} parqueos obtenidos.")
+            
+            Result.success()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Result.failure()
+        }
+    }
+}
